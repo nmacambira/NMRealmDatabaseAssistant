87cf93a554fa241d1fc65452f20080937e596795
Natalia <nmacambira@email.com>
2018-10-05 10:33:16 -0300

    add object exists

--- NMRealmDatabaseAssistant.swift
+++ NMRealmDatabaseAssistant.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2017 Natalia Macambira. All rights reserved.
 //
 
-import Realm
 import RealmSwift
 
-class NMRealmDatabaseAssistant {
+final class NMRealmDatabaseAssistant {
     
     static let realm = try! Realm()
+    
+    //MARK: - Object exist
+    static func objectExist <T: RealmSwift.Object>(id: Int, inEntity entity: T.Type) -> Bool {
+        return realm.object(ofType: entity, forPrimaryKey: id) != nil
+    }
     
     //MARK: - Save objects
     static func save(object: Object) {
@@ -57,47 +61,33 @@ class NMRealmDatabaseAssistant {
     
     //MARK: - Querys
     static func queryForAnArrayOf<T: RealmSwift.Object>(entity: T.Type, sortedByKeyPath keyPath: String?) -> Array<T>? {
-        
         if keyPath != nil {
-            
             return Array(realm.objects(entity).sorted(byKeyPath: keyPath!, ascending: true))
-            
         } else {
-            
             return Array(realm.objects(entity))
         }
     }
     
     static func queryForAnArrayOf<T: RealmSwift.Object>(entity: T.Type, withPredicate predicate: NSPredicate, sortedByKeyPath keyPath: String?) -> Array<T>? {
-        
         if keyPath != nil {
-            
             return Array(realm.objects(entity).filter(predicate).sorted(byKeyPath: keyPath!, ascending: true))
-            
         } else {
-            
             return Array(realm.objects(entity).filter(predicate))
         }
     }
     
     static func queryForSingleObjectFrom<T: RealmSwift.Object>(entity: T.Type) -> T? {
-        
         let result = realm.objects(entity)
-        
         return result.first
     }
     
     static func queryForSingleObjectFrom<T: RealmSwift.Object>(entity: T.Type, withIdentifier id: Int) -> T? {
-        
         let result = realm.objects(entity).filter("identifier == %d", id)
-        
         return result.first
     }
     
     static func queryForSingleObjectFrom<T: RealmSwift.Object>(entity: T.Type, withPredicate predicate: NSPredicate) -> T? {
-        
         let result = realm.objects(entity).filter(predicate)
-        
         return result.first
     }
     
